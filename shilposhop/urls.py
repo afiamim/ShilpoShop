bc5839850ca875e34c237707b0b4ef92a7f570c7
siammubc12-a11y <siammubc12@gmail.com>
2026-05-14 18:48:39 +0600

    update

--- shilposhop/urls.py
+++ shilposhop/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products_app.urls')),
+    path('orders/', include('orders_app.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
