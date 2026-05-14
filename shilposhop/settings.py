feabab980e5b57cda013938933e0473d8ce84d64
siammubc12-a11y <siammubc12@gmail.com>
2026-05-14 19:41:15 +0600

    order app

--- shilposhop/settings.py
+++ shilposhop/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'products_app',
     'cart_app',
     'users_app',
+    'orders_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
