5d6a94af11ef160299e4704e74a0e18b26b6b88f
jayebarai <23201186@uap-bd.edu>
2026-05-14 19:21:51 +0600

    users_app

--- shilposhop/settings.py
+++ shilposhop/settings.py
@@ -33,12 +33,14 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'products_app',
     'cart_app',
+    'users_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
